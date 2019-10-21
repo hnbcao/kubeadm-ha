f899d277769885bfa70ff7713697c315d57410ab
hnbcao <hnbcao@gmail.com>
2019-10-21 16:55:41 +0800

    Create install-docker-offical.sh

---
+++ install-docker-offical.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+if [ -z "`yum list installed | grep docker`" ] ;then
+    echo "============This System Have Not Install Docker !!!============"
+else
+    echo "============This System Have Install Docker !!!============"
+    exit 10
+fi
+echo "============Prepare Docker Install !!!============"
+yum install -y yum-utils device-mapper-persistent-data lvm2
+echo "============Add Docker Yum Repo !!!============"
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+echo "============Install Docker !!!============"
+yum install -y docker-ce-18.06.3.ce
+echo "============Set Up Docker !!!============"
+if [ -z "`grep "ExecStartPost=/usr/sbin/iptables -P FORWARD ACCEPT" /usr/lib/systemd/system/docker.service`" ] ;then
+    echo "============Skip Set Up Docker !!!============"
+    cat /usr/lib/systemd/system/docker.service
+else
+    sed -i "13i ExecStartPost=/usr/sbin/iptables -P FORWARD ACCEPT" /usr/lib/systemd/system/docker.service
+    cat /usr/lib/systemd/system/docker.service
+fi
+systemctl daemon-reload
+echo "============Start Docker !!!============"
+systemctl start docker
+echo "============Set Docker Auto Start !!!============"
+systemctl enable docker
